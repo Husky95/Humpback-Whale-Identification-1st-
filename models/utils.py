da69221d8a7e4577d72e2288a8130db5a3002dcd
Husky <kurst811@gmail.com>
2022-02-23 19:32:27 -0800

    test sigmoid

--- models/utils.py
+++ models/utils.py
@@ -87,7 +87,7 @@ def weights_init_classifier(m):
 def try_bestfitting_loss(results, labels, selected_num=10):
     batch_size, class_num = results.shape
     labels = labels.view(-1, 1)
-    one_hot_target = torch.zeros(batch_size, class_num + 1).cuda().scatter_(1, labels, 1)[:, :15587].contiguous()
+    one_hot_target = torch.zeros(batch_size, class_num ).cuda().scatter_(1, labels, 1)[:, :15587].contiguous()
     error_loss = lovasz_hinge(results, one_hot_target)
     labels = labels.view(-1)
     indexs_new = (labels != 15587).nonzero().view(-1)
@@ -104,15 +104,16 @@ def sigmoid_loss(results, labels, topk=10):
         results = results.view(1, -1)
     batch_size, class_num = results.shape
     labels = labels.view(-1, 1)
-    one_hot_target = torch.zeros(batch_size, class_num ).cuda().scatter_(1, labels, 1)[:, :15587*2]
+    one_hot_target = torch.zeros(batch_size, class_num ).cuda().scatter_(1, labels, 1)[:, :15587]
     #lovasz_loss = lovasz_hinge(results, one_hot_target)
     error = torch.abs(one_hot_target - torch.sigmoid(results))
     error = error.topk(topk, 1, True, True)[0].contiguous()
     target_error = torch.zeros_like(error).float().cuda()
     error_loss = nn.BCELoss(reduce=True)(error, target_error)
     labels = labels.view(-1)
-    indexs_new = (labels != 15587*2).nonzero().view(-1)
+    indexs_new = (labels != 15587).nonzero().view(-1)
     if len(indexs_new) == 0:
+        print("len(index_new == 0")
         return error_loss
     results_nonew = results[torch.arange(0, len(results))[indexs_new], labels[indexs_new]].contiguous()
     target_nonew = torch.ones_like(results_nonew).float().cuda()
